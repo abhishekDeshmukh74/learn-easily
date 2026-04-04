56cbb39211bcdc5bfb3efd1053c99c27ff7cfea9
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2026-04-05 01:42:18 +0530

    feat: add entry point main.tsx and minimal App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,8 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { ConceptViewer } from './components/ConceptViewer';
-import { HomePage } from './components/HomePage';
-import { ThemeProvider } from './lib/theme';
-
-// Import concept registry (auto-registers all concepts)
-import './concepts';
-
 function App() {
   return (
-    <ThemeProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/learn/:conceptId" element={<ConceptViewer />} />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+    <div>
+      <h1>Learn Easily</h1>
+    </div>
   );
 }
 
